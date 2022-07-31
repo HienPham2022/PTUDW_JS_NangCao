bfd4351e9467ad2711c4f7050aa147f253c451f6
hienpham <pvhien.ehs.2020@gmail.com>
2022-07-31 16:02:33 +0700

    Ex01

---
+++ 21880038_Ex01.js
@@ -0,0 +1,30 @@
+const _Class = [    
+      {
+        ID_student: "112345",
+        Name: "PVHien",
+        Courses:{
+              Course_Name:"JS",
+              Score:0  
+        }
+      },
+       {
+        ID_student: "223344",
+        Name: "LVToan",
+        Courses:{
+              Course_Name:"C#",
+              Score:0  
+        }
+      },
+       {
+        ID_student: "543211",
+        Name: "NTDung",
+        Courses:{
+              Course_Name:"AI",
+              Score:0  
+        }
+      }
+  ];
+   _Class.forEach(student => {
+        console.log(student)
+  });
+  
